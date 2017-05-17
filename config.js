11020ec55b4435b8cb50cfa8942f6f0c405387de
Noris Alsaadie <norisalsaadie@Noriss-MacBook-Pro.local>
2017-05-17 11:42:46 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+  oidc: {
+     oktaUrl: "-",
+     clientId: "-",
+     clientSecret: "-",
+     redirectUri: "http://localhost:8080/authorization"
+  },
+   port: process.env.PORT || 8080
+}
