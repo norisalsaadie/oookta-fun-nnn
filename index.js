91bee053063069506c0ed15f7b26c71abf89107f
Noris Alsaadie <norisalsaadie@Noriss-MacBook-Pro.local>
2017-05-21 16:21:50 +0100

    Server side route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser')
 const compression = require('compression')
 const express = require('express')
 const path = require('path')
+const jws = require('jws')
 const config = require('./config')
 
 const PORT = config.port
@@ -13,6 +14,20 @@ express()
   .set('view engine', 'html')
   .set('views', path.resolve(__dirname, 'views'))
   .get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'index.html')))
+  .get('/authenticated-yo', (req, res, next) => {
+    const auth = req.get('authorization').split(' ')[1]
+    if (!auth) {
+      return res.status(401).send('oh snap, you have no token. Please go home.')
+    }
+    const decoded = jws.decode(auth)
+
+    console.log(decoded);
+
+    if (!decoded) {
+      return res.status(401).send('id_token could not be decoded from the response')
+    }
+
+  })
   .listen(PORT, () => {
     console.log(`server up and listening on ${PORT}`)
   })
